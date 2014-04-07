464b9c494cbc4b5169dd197ef019967134811aa5
Stephen Soltesz <stephen.soltesz@gmail.com>
2014-04-07 00:47:39 -0400

    add support for recording profiling info.

--- cmds/lineviewer/server.go
+++ cmds/lineviewer/server.go
@@ -20,6 +20,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"runtime/pprof"
 
 	"github.com/stephen-soltesz/go/collection"
 	//"github.com/vdobler/chart"
@@ -31,10 +32,11 @@ var (
 	viewerPort    = flag.Int("viewer_port", 8080, "Port for web viewer.")
 	collectorPort = flag.Int("collector_port", 3131, "Port for data collector.")
 	timestamp     = flag.Bool("timestamp", false, "Use timestamps as x-axis.")
-	debug         = flag.Bool("debug", false, "Enable debug messages on stderr.")
 	plotWidth     = flag.Int("plot_width", 600, "Plot width in pixels.")
 	plotHeight    = flag.Int("plot_height", 400, "Plot height in pixels.")
+	debug         = flag.Bool("debug", false, "Enable debug messages on stderr.")
 	debugLogger   *log.Logger
+	profile       = flag.Bool("profile", false, "Enable profiling.")
 )
 
 func checkFlags() {
@@ -54,8 +56,17 @@ func main() {
 	flag.Parse()
 	checkFlags()
 
+	if *profile {
+		f, err := os.Create("lineviewer.prof")
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	collector := collection.Default()
-	go startCollectorServer(*bindAddress, *collectorPort, collector)
 	go startViewServer(*bindAddress, *viewerPort)
-	select {}
+	startCollectorServer(*bindAddress, *collectorPort, collector)
+	//select {}
 }
