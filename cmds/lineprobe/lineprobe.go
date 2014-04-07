9adcbc60c755a1a61c9b7e9f6c2876cbba0f009e
Stephen Soltesz <stephen.soltesz@gmail.com>
2014-04-07 00:47:07 -0400

    add support for operations (avg, percentile) on raw data.

--- cmds/lineprobe/lineprobe.go
+++ cmds/lineprobe/lineprobe.go
@@ -11,15 +11,18 @@ Options
 
 These are command-line options.
 
-    -hostname=localhost:3131       Host and port of view server.
-    -interval=1.0                  How often to run command.
-    -command=                      Command to run every second.
-    -axis=default                  Name of axis to associate this line.
-    -xlabel=                       X-Label for axis.
-    -ylabel=                       Y-Label for axis.
-    -label=                        Line label name on axis.
-    -color=                        Color of line. Chosen automatically by default.
-    -q                             Silence the echo of values sent to the server.
+    -hostname=localhost:3131  Host and port of view server.
+    -interval=1.0             How often to run command.
+    -command=                 Command to run every second.
+    -axis=default             Name of axis to associate this line.
+    -xlabel=                  X-Label for axis.
+    -ylabel=                  Y-Label for axis.
+    -label=                   Line label name on axis.
+    -color=                   Color of line. Chosen automatically by default.
+    -q                        Silence the echo of values sent to the server.
+    -samples=                 Number of samples to use when calculating functions.
+    -avg=false                Whether to report the average of the number of samples.
+    -percentile=<pct>         Percentile to report over number of samples.
 
 Examples
 
@@ -34,12 +37,17 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
+	"math"
 	"os"
 	"os/exec"
+	"sort"
+	"strconv"
 	"time"
 
 	// third-party
@@ -56,10 +64,200 @@ var (
 	lineName    = flag.String("label", "", "Line label name on axis.")
 	lineColor   = flag.String("color", "", "Color of line. Chosen automatically by default.")
 	quietOutput = flag.Bool("q", false, "Whether to echo values sent to server.")
+	exitServer  = flag.Bool("exit", false, "Tell the lineviewer to exit.")
+
+	calcOperations operationSlice
+
+	debug         = flag.Bool("debug", false, "Enable debug messages on stderr.")
+	debugLogger   *log.Logger
 )
+
+type sampleSet struct {
+	samples []float64
+	pos int
+}
+
+type ValueWriter struct {
+	writer *bufio.ReadWriter
+	server *lineserver.Server
+	op *Operation
+	samples *sampleSet
+}
 
 type ValueReader struct {
 	reader *bufio.Reader
+	cmd *exec.Cmd
+}
+
+func initFlags() {
+	flag.Var(&calcOperations, "operation", "An operation to run on samples.")
+}
+
+func checkFlags() {
+	if *debug {
+		debugLogger = log.New(os.Stderr, "", 0)
+	} else {
+		debugLogger = log.New(ioutil.Discard, "", 0)
+	}
+	if len(calcOperations) == 0 {
+		calcOperations = append(calcOperations, &Operation{OpPercentile, 1, 50})
+	}
+}
+
+func printAllFlagsAndValues() {
+	flag.VisitAll(func(f *flag.Flag) {
+		debugLogger.Printf("%-30s %s\n", fmt.Sprintf("-%s=%s", f.Name, f.Value), f.Usage)
+	})
+}
+
+func newSampleSet(sampleCount int64) *sampleSet {
+	return &sampleSet{make([]float64, sampleCount), 0}
+}
+
+func (ss *sampleSet) Count() int {
+	if ss.Full() {
+		return len(ss.samples)
+	} else {
+		return ss.pos
+	}
+}
+
+func (ss *sampleSet) Full() bool {
+	return ss.pos >= len(ss.samples)-1
+}
+
+func (ss *sampleSet) Append(val float64) {
+	ss.samples[ss.pos%len(ss.samples)] = val
+	ss.pos += 1
+}
+
+func (ss *sampleSet) Mean() float64 {
+	count := ss.Count()
+	var s float64 = 0
+	for i := 0; i < count; i++ {
+		s += ss.samples[i]
+	}
+	avg := s/float64(count)
+	debugLogger.Println("avg:", avg)
+	return avg
+}
+
+func (ss *sampleSet) Stdev() float64 {
+	count := ss.Count()
+	avg := ss.Mean()
+	var variance float64 = 0
+
+	for i := 0; i < count; i++ {
+		variance += math.Pow((ss.samples[i] - avg), 2)
+	}
+	stdev := math.Sqrt(variance/float64(count))
+	debugLogger.Println("stdev:", stdev)
+	return stdev
+}
+
+func (ss *sampleSet) Percentile(pct int64) float64 {
+	count := ss.Count()
+	var calc = make([]float64, count)
+	copy(calc, ss.samples)
+
+	i := int(math.Ceil(float64(count-1) * float64(pct)/101.0))
+	sort.Float64s(calc)
+	debugLogger.Println("size", count, "i", i)
+	debugLogger.Println("pct:", calc[i])
+	return calc[i]
+}
+
+func NewValueWriter(op *Operation) *ValueWriter {
+	var err error
+	writer := ValueWriter{}
+	writer.op = op
+	fmt.Printf("%#v\n", op)
+	writer.samples = newSampleSet(op.samples)
+	writer.server = lineserver.NewServer(*hostname)
+	if writer.writer, err = writer.server.Connect(); err != nil {
+		log.Fatal(err)
+	}
+	return &writer
+}
+
+func SetupValueWriters() []*ValueWriter {
+	var writers []*ValueWriter
+	for _, op := range calcOperations {
+		writer := NewValueWriter(op)
+		if err := writer.sendClientSettings(); err != nil {
+			log.Fatal(err)
+		}
+		writers = append(writers, writer)
+	}
+	return writers
+}
+
+func (w *ValueWriter) SendValue(val float64) error {
+	var f float64
+	w.samples.Append(val)
+	if w.op.operation == OpMean {
+		f = w.samples.Mean()
+	} else if w.op.operation == OpStdev {
+		f = w.samples.Stdev()
+	} else if w.op.operation == OpPercentile {
+		f = w.samples.Percentile(w.op.percentile)
+	}
+
+	out := formatFloat(f)
+	fmt.Printf("Sending: %s", string(out))
+	_, err := w.writer.Write(out)
+	if err != nil {
+		return err
+	}
+	w.writer.Flush()
+	return nil
+}
+
+func (w *ValueWriter) sendClientSettings() error {
+	if *exitServer {
+		fmt.Println("Sending exit")
+		_, err := w.writer.WriteString("EXIT\n")
+		if err != nil {
+			return err
+		}
+		w.writer.Flush()
+		// create new connection to trigger exit.
+		time.Sleep(time.Second)
+		NewValueWriter(&Operation{OpNone, 0, 0})
+		os.Exit(0)
+	}
+	axisSetting := "axis:" + *axisName + ":" + *xlabelName + ":" + *ylabelName + "\n"
+	_, err := w.writer.WriteString(axisSetting)
+	if err != nil {
+		return err
+	}
+
+	name := ""
+	if w.op.operation > OpNone {
+		name = fmt.Sprintf("%s-", w.op)
+	}
+
+	if *lineName != "" {
+		name = "label:" + name + *lineName + "\n"
+	} else if *command != "" {
+		name = "label:" + name + *command + "\n"
+	} else {
+		name = "label:" + name + fmt.Sprintf("Thread-%d\n", os.Getpid())
+	}
+
+	_, err = w.writer.WriteString(name)
+	if err != nil {
+		return err
+	}
+
+	if *lineColor != "" {
+		colorSetting := fmt.Sprintf("color:%s\n", *lineColor)
+		_, err := w.writer.WriteString(colorSetting)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func NewValueReader() *ValueReader {
@@ -74,90 +272,91 @@ func (r *ValueReader) Setup() error {
 	if fromStdin {
 		r.reader = bufio.NewReader(os.Stdin)
 	} else {
-		cmd := exec.Command("bash", "-c", *command)
-		output, err := cmd.StdoutPipe()
+		r.cmd = exec.Command("bash", "-c", *command)
+		output, err := r.cmd.StdoutPipe()
 		if err != nil {
 			return err
 		}
-		if err := cmd.Start(); err != nil {
+		if err := r.cmd.Start(); err != nil {
 			return err
 		}
-		go cmd.Wait()
 		r.reader = bufio.NewReader(output)
 	}
 	return nil
 }
 
-func (r *ValueReader) GetValue() []byte {
+func (r *ValueReader) ReadValue() (float64, error) {
 	var err error
 	var lineTooLong bool
 	var out []byte
 
 	out, lineTooLong, err = r.reader.ReadLine()
-	out = append(out, '\n')
 	if lineTooLong {
-		return nil
+		return 0.0, errors.New("line too long")
 	} else if err == io.EOF {
+		r.cmd.Wait()	// must run after EOF to avoid races with Read()
 		time.Sleep(time.Duration(*interval) * time.Second)
 		err := r.Setup()
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println("reader setup failed:", err)
+			return 0.0, err
 		}
-		return r.GetValue()
+		return r.ReadValue()
 	} else if err != nil {
-		log.Fatal(err)
+		fmt.Println("unknown failure:", err)
+		return 0.0, err
 	}
-	return out
-}
 
-func SendValue(writer io.Writer, out []byte) {
-	_, err := writer.Write(out)
+	f, err := strconv.ParseFloat(string(out), 64)
 	if err != nil {
-		log.Fatal(err)
+		return 0.0, err
 	}
+	if !*quietOutput {
+		fmt.Printf("Received: %f\n", f)
+	}
+	return f, nil
 }
 
-func sendClientSettings(writer *bufio.ReadWriter) {
-	axisSetting := "axis:" + *axisName + ":" + *xlabelName + ":" + *ylabelName + "\n"
-	SendValue(writer, []byte(axisSetting))
-	if *lineName != "" {
-		SendValue(writer, []byte(fmt.Sprintf("label:%s\n", *lineName)))
+func parseFloat(val []byte) float64 {
+	fmt.Println("%v", val)
+	f, err := strconv.ParseFloat(string(val), 64)
+	if err != nil {
+		debugLogger.Println(err)
+		return 0.0
 	}
-	if *lineColor != "" {
-		SendValue(writer, []byte(fmt.Sprintf("color:%s\n", *lineColor)))
-	}
+	return f
+}
+
+func formatFloat(f float64) []byte {
+	return []byte(fmt.Sprintf("%f\n", f))
 }
 
 func main() {
-	var writer *bufio.ReadWriter
-	var out []byte
 	var err error
+	var val float64
 
+	initFlags()
 	flag.Parse()
+	checkFlags()
+
 	if !*quietOutput {
-		flag.VisitAll(func(f *flag.Flag) {
-			fmt.Printf("%-30s %s\n", fmt.Sprintf("-%s=%s", f.Name, f.Value), f.Usage)
-		})
+		printAllFlagsAndValues()
 	}
 
-	server := lineserver.NewServer(*hostname)
-	if writer, err = server.Connect(); err != nil {
-		log.Fatal(err)
-	}
-
+	// allocate value reader and writers.
 	reader := NewValueReader()
-	sendClientSettings(writer)
+	writers := SetupValueWriters()
 
 	for {
-		if out = reader.GetValue(); out == nil {
-			continue
+		if val, err = reader.ReadValue(); err != nil {
+			log.Println(err)
+			break
 		}
-		if !*quietOutput {
-			fmt.Print("Sending: ", string(out))
+		for _, writer := range writers {
+			if err = writer.SendValue(val); err != nil {
+				log.Fatal(err)
+			}
 		}
-		SendValue(writer, out)
-		writer.Flush()
 	}
-
 	return
 }
