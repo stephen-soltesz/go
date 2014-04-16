4be4ba8f879a209a5f01c34f43c0e5cd5504143c
Stephen Soltesz <stephen.soltesz@gmail.com>
2014-04-16 01:43:57 -0400

    improve documentation for godoc.

--- cmds/lineviewer/server.go
+++ cmds/lineviewer/server.go
@@ -6,6 +6,41 @@ clients connect and send data corresponding to data streams. The second port is
 a simple http server that allows multiple viewers of the collected data in
 real-time.
 
+Command Options
+
+Lineviewer runs two servers on different ports. The first is the collector.
+Lineprobe clients connect directly to the connector port. The second port is
+the display and shows the image via a simple http server.
+
+By default, lineviewer binds to all interfaces. This permits lineprobe clients
+from other systems, or for viewers on other systems.
+
+  -hostname=0.0.0.0
+  -viewer_port=8080         Port for web viewer.
+  -collector_port=3131      Port for data collector.
+
+The server also has an option for addition debug information. And, you can
+enable collection of profiling information.
+
+  -debug
+  -profile                  Writes to lineviewer.prof.
+
+Plot Options
+
+By default, the lineviewer treats the X-axis as the sequence number of each
+sample. However, the lineviewer can track the timestamp of when each value is
+received.
+
+  -timestamp                Enable timestamp collection for x-axis.
+  -plot_width=<int>         Width in pixels.
+  -plot_height=<int>        Height in pixels.
+
+Examples
+
+An example of invoking lineviewer.
+
+   lineviewer --timestamp --plot_width 800 --plot_height 600
+
 */
 package main
 
@@ -17,8 +52,8 @@ import (
 	"os"
 	"runtime/pprof"
 
-	"github.com/stephen-soltesz/go/collection"
 	"github.com/stephen-soltesz/chart"
+	"github.com/stephen-soltesz/go/collection"
 )
 
 var (
