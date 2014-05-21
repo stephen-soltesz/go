c0521472c1f3474ecde70a58a64731b5f281d20e
Stephen Soltesz <stephen.soltesz@gmail.com>
2014-05-20 23:36:37 -0400

    add const tokens for collector configuration.

---
+++ collection/token/token.go
@@ -0,0 +1,15 @@
+/* This package defines constant tokens for the collection protocol. */
+package token
+
+const (
+	Axis        = "axis"
+	AxisScale   = "axis-scale"
+	AxisXLimit  = "axis-xlimit"
+	AxisYLimit  = "axis-ylimit"
+	LineLabel   = "line-label"
+	LineColor   = "line-color"
+	Exit        = "exit"
+	Reset       = "reset"
+	ScaleLog    = "log"
+	ScaleLinear = "linear"
+)
